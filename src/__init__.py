d48fc715770726e44608b86bfd95fa58847fcaef
maris333 <animaral@gmail.com>
2023-11-03 19:06:52 +0100

    Add precommit hook

--- src/__init__.py
+++ src/__init__.py
@@ -7,8 +7,15 @@ from src.config import Config
 db = SQLAlchemy()
 ma = Marshmallow()
 
+
 def create_app():
-    from src.views import read_blueprint, create_blueprint, edit_blueprint, delete_blueprint, check_blueprint
+    from src.views import (
+        read_blueprint,
+        create_blueprint,
+        edit_blueprint,
+        delete_blueprint,
+        check_blueprint,
+    )
 
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -23,6 +30,7 @@ def create_app():
     app.register_blueprint(delete_blueprint)
 
     return app
+
 
 def create_db(app):
     with app.app_context():
